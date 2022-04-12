f68e61ae577a68462c0c319273095d135918f202
JRM <johnrhmartin@gmail.com>
2022-04-11 21:46:50 -0400

    added jobsearch.js

---
+++ src/components/jobsearch.js
@@ -0,0 +1,24 @@
+import React from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { increment, decrement } from '../actions';
+
+function JobSearch() {
+    return (
+        <div>
+            <div class="row justify-content-center mt-5">
+                <div class="col-12 col-md-10 col-lg-8 mt-5">
+                    <div class="col-auto">
+                        <button class="btn btn-success" type="submit">Search</button>
+                    </div>
+                    <h1 id="jobs-in-area">Showing jobs in your area</h1>
+                    <div class="mt-2">
+                        {/* {{> jobs-card jobs}} */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default JobSearch;
+

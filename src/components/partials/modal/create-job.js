7070b12639d3292623fb5e9e51f34ae2e491bdb2
JRM <johnrhmartin@gmail.com>
2022-04-11 23:55:28 -0400

    added create job fetch request, but there is a bug with timeframe

--- src/components/partials/modal/create-job.js
+++ src/components/partials/modal/create-job.js
@@ -1,6 +1,57 @@
 import React from 'react';
+import axios from 'axios';
 // import { useSelector, useDispatch } from 'react-redux';
 // import { increment, decrement } from '../actions';
+
+function theBtn() {
+    // var modal = document.getElementById("exampleModal");
+    // modal.style.display = "block";
+    // console.log("id: ", id, " first_name: ", first_name);
+
+    async function createPostHandler() {
+      
+        const payStr = document.querySelector('input[name="pay"]').value.trim();
+        const pay = parseInt(payStr);
+
+        const dateStart = document.getElementById("date-start").value;
+        const hourStart = document.getElementById("hour-start").value;
+        const timeframe = dateStart + "T" + hourStart + "Z";
+
+        const locationStr = document
+            .querySelector('input[name="location"]')
+            .value.trim();
+        const location = parseInt(locationStr);
+
+        var visitType = document.getElementById("visit-type");
+        var selectedValue = visitType.options[visitType.selectedIndex].value;
+        if (selectedValue == "Walk") {
+            walk = true;
+            check_in = false;
+        } else {
+            walk = false;
+            check_in = true;
+        }
+
+        if ((pay, location)) {
+            const res = await axios.post('http://localhost:3002/api/jobs', 
+                {
+                    pay: pay,
+                    check_in: check_in,
+                    walk: walk,
+                    timeframe: timeframe, 
+                    location: location,
+                    completed: false,
+                    owner_id: 1,
+                    walker_id: null,
+                    animal_id: 1,
+                }
+            );
+            return res.data.json;
+        }
+    }
+    createPostHandler();
+        
+}
 
 function CreateJob() {
     console.log("CREATED");
@@ -38,7 +89,7 @@ function CreateJob() {
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-primary">Save changes</button>
+                            <button type="submit" class="btn btn-primary" onClick={() => theBtn()}>Save changes</button>
                         </div>
                     </div>
                     </div>
